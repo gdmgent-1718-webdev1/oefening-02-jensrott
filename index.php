9d8f0a0a1eccf1b7c2d3f5cb341eef1d059cf968
jensrott <jensrott@student.arteveldehs.be>
2017-10-17 22:04:06 +0200

    Push

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 
-echo "test";
+$String1 = "Hello";
+$String2 = "World";
+$Strings = "{$String1} {$String2}";
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Oefening 2</title>
+</head>
+<body>
+    <h1>De twee strings: <?=$Strings?></h1>
+</body>
+</html>
